64d5976ffa44c7fa22a18772f1faf9f5c2020e58
PedroHaratani <shunzoh2011@hotmail.com>
2021-04-21 23:35:48 -0300

    'projeto'

---
+++ BuffetLista1/Models/Buffet/Cliente/ClienteEntity.cs
@@ -0,0 +1,22 @@
+﻿using BuffetLista1.Models.Buffet.Evento;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuffetLista1.Models.Buffet.Cliente
+{
+    public class ClienteEntity
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public ICollection<EventoEntity> Eventos { get; set; }
+
+        public ClienteEntity()
+        {
+            Id = new Guid();
+            Eventos = new List<EventoEntity>();
+        }
+    }
+}
---
+++ BuffetLista1/Models/Buffet/Evento/EventoEntity.cs
@@ -0,0 +1,20 @@
+﻿using BuffetLista1.Models.Buffet.Cliente;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuffetLista1.Models.Buffet.Evento
+{
+    public class EventoEntity
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public ClienteEntity Cliente { get; set; }
+
+        public EventoEntity()
+        {
+            Id = new Guid();
+        }
+    }
+}
