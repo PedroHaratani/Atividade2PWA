e860f66b1923bd8268d539eea767d2275a5aae6d
PedroHaratani <shunzoh2011@hotmail.com>
2021-04-25 17:03:27 -0300

    APS

--- BuffetLista1/Models/Buffet/Cliente/ClienteEntity.cs
+++ BuffetLista1/Models/Buffet/Cliente/ClienteEntity.cs
@@ -11,12 +11,35 @@ namespace BuffetLista1.Models.Buffet.Cliente
         public Guid Id { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
+        public string Endereco{ get; set; }
+        public DateTime dataCadastro { get; set; }
+        public DateTime dataModificacao { get; set; }
+        public  Guid TipoCliente { get; set; }
+        public string Cpf { get; set; }
+        public  DateTime DataNascimento { get; set; }
+        public string CNPJ { get; set; }
+
         public ICollection<EventoEntity> Eventos { get; set; }
 
         public ClienteEntity()
         {
             Id = new Guid();
             Eventos = new List<EventoEntity>();
+        }
+
+        public ClienteEntity(Guid id, string nome, string email, string endereco, DateTime dataCadastro, DateTime dataModificacao, Guid tipoCliente, string cpf, DateTime dataNascimento, string cNPJ, ICollection<EventoEntity> eventos)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Endereco = endereco;
+            this.dataCadastro = dataCadastro;
+            this.dataModificacao = dataModificacao;
+            TipoCliente = tipoCliente;
+            Cpf = cpf;
+            DataNascimento = dataNascimento;
+            CNPJ = cNPJ;
+            Eventos = eventos;
         }
     }
 }
--- BuffetLista1/Models/Buffet/Evento/EventoEntity.cs
+++ BuffetLista1/Models/Buffet/Evento/EventoEntity.cs
@@ -8,13 +8,41 @@ namespace BuffetLista1.Models.Buffet.Evento
 {
     public class EventoEntity
     {
-        public Guid Id { get; set; }
-        public string Nome { get; set; }
-        public ClienteEntity Cliente { get; set; }
-
         public EventoEntity()
         {
-            Id = new Guid();
         }
+
+        public EventoEntity(Guid id, Guid tipoEvento, string descricao, DateTime dataInicioEvento, DateTime dataFimEvento, ClienteEntity clienteEvento, SituacaoEvento situacaoEvento, Local local, string observacoes, ClienteEntity cliente, DateTime dataInsercao, DateTime dataModificacao)
+        {
+            Id = id;
+            TipoEvento = tipoEvento;
+            Descricao = descricao;
+            this.dataInicioEvento = dataInicioEvento;
+            this.dataFimEvento = dataFimEvento;
+            this.clienteEvento = clienteEvento;
+            this.situacaoEvento = situacaoEvento;
+            this.local = local;
+            this.observacoes = observacoes;
+            Cliente = cliente;
+            this.dataInsercao = dataInsercao;
+            this.dataModificacao = dataModificacao;
+        }
+
+        public Guid Id { get; set; }
+        public Guid TipoEvento { get; set; }
+        public string Descricao { get; set; }
+        public DateTime dataInicioEvento {get;set;}
+        public DateTime dataFimEvento { get; set; }
+
+        public ClienteEntity clienteEvento { get; set; }
+        public SituacaoEvento situacaoEvento { get; set; }
+        public Local local { get; set; }
+        public string observacoes { get; set; }
+        public ClienteEntity Cliente { get; set; }
+        public  DateTime dataInsercao { get; set; }
+        public DateTime dataModificacao { get; set; }
+
+        
+
     }
 }
